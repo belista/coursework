cdff967b2938b499c351936058b1ae879ae87e2a
belista <shestakov9999@mail.ru>
2018-11-23 15:36:49 +0300

    Добавьте файлы проекта.

---
+++ coursework/Models/Organizations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace coursework.Models
+{
+    public class Organizations
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ChiefFirstName { get; set; }
+
+        public string ChiefFamilyName { get; set; }
+
+        public string ChiefPatronymic { get; set; }
+
+        public string Address { get; set; }
+
+        public int Phone { get; set; }
+
+        public int BIC { get; set; }
+
+        public int BankAccount { get; set; }
+    }
+}
