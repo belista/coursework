d89f42c952ccda02b18e7ea155826cee46cbd170
belista <shestakov9999@mail.ru>
2018-12-01 23:25:18 +0300

    added countriesController and views.

---
+++ coursework/Controllers/CountriesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using coursework.Models;
+
+namespace coursework.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private AgencyContext _db = new AgencyContext();
+        
+        public ActionResult Index()
+        {
+            return View(_db.Countries.ToList());
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Countries.Add(country);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(country);
+        }
+
+        [HttpGet]
+        public ActionResult Search(string search)
+        {
+            var result = _db.Countries.Where(c => c.Name.Contains(search)).ToList();
+
+            if (result.Count != 0)
+            {
+                ViewData.Model = result;
+            }
+            else
+            {
+                ViewData.Model = _db.Countries;
+            }
+
+            return View("~/Views/Countries/Index.cshtml");
+        }
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var country = _db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(country).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(country);
+        }
+        
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var country = _db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var country = _db.Countries.Find(id);
+            _db.Countries.Remove(country);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
