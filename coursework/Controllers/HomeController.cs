e2a59efb3805426fef2f0d9ec85b490014504ce4
belista <shestakov9999@mail.ru>
2018-12-01 23:23:45 +0300

    edited home controller and views.

--- coursework/Controllers/HomeController.cs
+++ coursework/Controllers/HomeController.cs
@@ -1,38 +1,18 @@
 ﻿using coursework.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace coursework.Controllers
 {
+    [RoutePrefix("home")]
     public class HomeController : Controller
     {
-        private AgencyContext _db;
-
-        public HomeController()
-        {
-            _db = new AgencyContext();
-        }
-
         public ActionResult Index()
         {
-            _db.Countries.Add(new Country { Name = "USA" });
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
